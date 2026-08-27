3b079a32d8b3ae72f013e0939824190d350d9fdb
fckoffmw <ekaterina.e.kopylova@icloud.com>
2026-08-27 16:42:07 +0300

    Installing the react and react-dom libraries, reconfiguring vite-config and vite dependencies

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   publicDir: 'static',
@@ -33,4 +34,5 @@ export default defineConfig({
       },
     },
   },
+  plugins: [react()],
 });
